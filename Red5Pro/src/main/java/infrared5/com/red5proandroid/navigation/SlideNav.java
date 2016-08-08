543b396025d19ca2ff74bcfb9db0a02fadce3b63
David Heimann <dheimann@infrared5.com>
2016-08-08 10:14:10 -0400

    added screen

---
+++ Red5Pro/src/main/java/infrared5/com/red5proandroid/navigation/SlideNav.java
@@ -0,0 +1,11 @@
+package infrared5.com.red5proandroid.navigation;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by davidHeimann on 8/5/16.
+ */
+public class SlideNav extends DialogFragment {
+
+
+}
